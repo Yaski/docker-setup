2b949a82547cc151051c334c5d9b9947261b43d8
Andrei Kopysov <yaskpp@gmail.com>
2018-08-08 17:40:40 +0300

    Main calls

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
 const _ = require('underscore');
+const Influx = require('influx');
+const asyncRedis = require("async-redis");
 
-console.log('Start');
+async function test() {
+  let influxdb = new Influx.InfluxDB({
+    host: "127.0.0.1",
+    port: "8086",
+    username: "",
+    password: "",
+    database: "test"
+   });
+
+  let redis = asyncRedis.createClient();
+
+  let rValue = await redis.get('test');
+  console.log('Old redis value', rValue);
+
+  await redis.set('test', 100 * Math.random());
+
+  await influxdb.writeMeasurement('one', [{tags: {}, fields:{
+    field: 5000 * Math.random()
+  }}]);
+
+  let rawData = await influxdb.queryRaw(`SELECT * FROM 'test.autogen.one' WHERE time > now() - 1h`);
+  console.log('Influx value');
+  console.log(rawData);
+}
+
+test();
